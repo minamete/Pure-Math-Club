d605ddbc25e1cadc58c4b384bf369d028478c0a7
minamete <andromefingercorn@gmail.com>
2022-05-16 11:32:18 -0400

    Added some sql stuff

--- qr/qrgen.js
+++ qr/qrgen.js
@@ -1,11 +1,12 @@
 require('dotenv').config() // just for testing; make sure to move .env into root directory
 const crypto = require('crypto')
+const mysql = require('mysql2')
 
 // "this is so insane and overengineered" - evan
 var encrypt = (key, text) => {
-    var secret = crypto.randomBytes(32)
+    var secret = crypto.randomBytes(16)
     var iv = crypto.randomBytes(16)
-    var cipher = crypto.createCipheriv('aes-256-cbc', secret, iv)
+    var cipher = crypto.createCipheriv('aes-128-cbc', secret, iv)
 
     var message = {
         text: text,
@@ -15,7 +16,24 @@ var encrypt = (key, text) => {
     
     let encrypted = cipher.update(JSON.stringify(message), 'utf8', 'hex');
     encrypted += cipher.final('hex');
-    console.log(encrypted)
+    return encrypted
 }
 
-encrypt(process.env.PUBLIC_KEY, "hello")
+var createItem = () => {
+    // Create the str and encrypt it
+    var str = crypto.randomBytes(16).toString('hex')
+    var encrypted = encrypt(process.env.PUBLIC_KEY, str)
+
+    // Add the encrypted str to the database
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: process.env.MYSQL_PASSWORD,
+    })
+
+    connection.connect((err) => {if (err) throw err; console.log('connected')})
+
+    // Create the QR code from the original string
+}
+
+createItem()
